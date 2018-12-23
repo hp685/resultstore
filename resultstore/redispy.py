16d8dab4ac00ae041a00048929a5fecebc82f5ee
hp685 <harsh@nyu.edu>
2018-12-22 16:35:36 -0800

    redis consumer and producer

---
+++ resultstore/redispy.py
@@ -0,0 +1,53 @@
+
+from redis import StrictRedis
+
+from time import sleep
+
+from base import BaseConsumer, BaseProducer
+
+_DEFAULT_HOST = 'localhost'
+_DEFAULT_PORT = 6379
+_RESULT_STORE = 'result-store'
+
+
+def _task_id(task_id):
+    return '{0}_{1}'.format(_RESULT_STORE, task_id)
+
+
+class RedisProducer(BaseProducer):
+
+    def __init__(self, task_id, expiry=None, host=_DEFAULT_HOST, port=_DEFAULT_PORT, serialization='dill'):
+        self.host = host
+        self.port = port
+        self.client = StrictRedis(host=self.host, port=self.port)
+        self.task_id = _task_id(task_id)
+        self.expiry = expiry
+        self.body = None
+        super(BaseProducer, self).__init__(serialization=serialization)
+
+    def send_message(self, body):
+        self.body = self._serialize(body)
+        if self.expiry:
+            self.client.setex(self.task_id, self.expiry, self.body)
+        else:
+            self.client.set(self.task_id, self.body)
+
+
+class RedisConsumer(BaseConsumer):
+
+    def __init__(self, task_id, host=_DEFAULT_HOST, port=_DEFAULT_PORT, serialization='dill'):
+        self.host = host
+        self.port = port
+        self.client = StrictRedis(host=self.host, port=self.port)
+        self.task_id = _task_id(task_id)
+        super(BaseConsumer, self).__init__(serialization=serialization)
+
+    def get(self, polling_interval=0.1):
+        while True:
+            result = self.client.get(self.task_id)
+            if result:
+                if self.client.ttl(self.task_id):
+                    return result
+                self.client.delete(self.task_id)
+                return result
+            sleep(polling_interval)
