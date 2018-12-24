6a108c88b2bc28124b2b92abb9ae251ab982556e
hp685 <Harsh Patel>
2018-12-24 01:31:54 -0800

    update contact and url

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setup(
     name='resultstore',
     version='0.1',
     packages=['resultstore'],
+    author="Harsh Patel",
+    author_email="harsh@nyu.edu",
+    url='https://github.com/hp685/resultstore',
     long_description=open('README.md').read()
 )
